213af60534c49dd351645d0c4bf1a73b5bd40b8b
Avishek-kr <ak251997@gmail.com>
2021-03-06 19:19:08 +0530

    HTML/CSS of index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+
+const body= document.body;
+const btn=document.getElementById('btn');
+const color = document.querySelector('.color')
+
+btn.addEventListener('click',function(){
+	let randomNumber=getRandomNumber();
+	body.style.backgroundColor = colors[randomNumber]; 
+
+})
+
+
+function getRandomNumber(){
+	return Math.floor(Math.random()*colors.length);
+}
